23f7dfc0344c86754334909073e94752b76b37d0
S_Kono <siiuiaemtkki@gmail.com>
2025-05-11 18:05:29 +0900

    add:Spring Bootプロジェクト初回commit

---
+++ src/test/java/com/example/diet/DietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.diet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

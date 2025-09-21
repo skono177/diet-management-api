8197513d63b617a69843de2d4df7ee71bec9b4aa
S_Kono <siiuiaemtkki@gmail.com>
2025-09-21 17:49:23 +0900

    AWSへの認証情報の設定方法を修正

--- src/main/java/com/example/diet/repository/s3/S3ClientProvider.java
+++ src/main/java/com/example/diet/repository/s3/S3ClientProvider.java
@@ -3,8 +3,7 @@ package com.example.diet.repository.s3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -15,17 +14,13 @@ public class S3ClientProvider {
     private final String bucketName;
 
     public S3ClientProvider(
-        @Value("${aws.s3.access-key}") String accessKey,
-        @Value("${aws.s3.secret-key}") String secretKey,
         @Value("${aws.s3.bucket-name}") String bucketName,
         @Value("${aws.s3.region}") String region) {
         this.bucketName = bucketName;
 
-        AwsBasicCredentials creds = AwsBasicCredentials.create(accessKey,
-            secretKey);
         this.s3Client = S3Client.builder()
             .region(Region.of(region))
-            .credentialsProvider(StaticCredentialsProvider.create(creds))
+            .credentialsProvider(DefaultCredentialsProvider.create())
             .build();
     }
 
